aa701d010ae1eaf3917ad4cd3346345dc6e756bd
shifufu <shiff@iterbao.net>
2019-03-02 15:45:56 +0800

    aaa

--- itcast-springboot/src/main/java/com/sf/Main.java
+++ itcast-springboot/src/main/java/com/sf/Main.java
@@ -6,6 +6,7 @@ import com.sf.application.BootApplication;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.print("哈哈哈");
 		SpringApplication.run(BootApplication.class, args);
 	}
 }
